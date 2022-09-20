a7f6523834e2c0a04c1d2fa23a2e550dd754b4c5
Hajar Aswad <hajaraswadkom@gmail.com>
2022-09-20 21:42:08 +0700

    update model, add helper uuid and response formatter

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Helpers\Uuid;
+use Carbon\Traits\Timestamp;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Uuid, Timestamp, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
--- app/Models/UserRedeem.php
+++ app/Models/UserRedeem.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Helpers\Uuid;
+use Carbon\Traits\Timestamp;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserRedeem extends Model
 {
-    use HasFactory;
+    use HasFactory,Uuid, Timestamp, SoftDeletes;
 }
